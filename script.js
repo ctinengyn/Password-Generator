15c5272ffe585f17793c674b432c43233998a8a2
Christine Nguyen <ctine.ngyn@gmail.com>
2021-02-17 02:43:52 -0800

    Added variables for number, symbols, lowercase/uppercase characters and user input

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+// Variables for different choices for password
+var number = '0123456789';
+var symbols = '!@#$%^&*?';
+var lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+var uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+// Users choice of length and variables
+var userChoice = {}; 
+
+// Used to determine the length of password
+var length = parseInt(prompt("Please type in a number between 8 and 128"));
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
+
+// Get user input
+var numberQuestion = confirm('Do you want numbers? Click OK for yes, cancel for no')
+
+var symbolQuestion = confirm('Do you want symbols? Click OK for yes, cancel for no')
+
+var lowercaseQuestion = confirm('Do you want lowercase characters? Click OK for yes, cancel for no')
+
+var uppercaseQuestion = confirm('Do you want uppercase characters? Click OK for yes, cancel for no')
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +33,10 @@ function writePassword() {
 
 }
 
-var number = '0123456789';
-var abc = 'abcdefghijklmnopqrstuvwxyz'; 
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
