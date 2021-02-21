150f12a9dd61fcca143175b142b0b7c2aebb034f
Christine Nguyen <ctine.ngyn@gmail.com>
2021-02-20 22:07:56 -0800

    Redoing prompt questions and adding more inputs

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
 // Variables for different choices for password 'strings'
-var number = '0123456789';
+var numbers = '0123456789';
 var symbols = '!@#$%^&*?';
 var lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
 var uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 // Users choice of length and variables
-var userChoice = {}; 
+var userChoice = {};
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// #generate button from html and css 
-
-var length = parseInt(prompt('How many characters do you want to be in your password?'))
-
-if (length < 8) {
-  prompt('How many characters do you want to be in your password?');
-
-  }else if (length > 8); {
-
-}
+// #generate button from html and css
 
 // Used to determine the length of password
 function length() {
 
-  var userChoice = parseInt(prompt('How many characters do you want to be in your password?'));
+  var userChoice = parseInt(window.prompt('How many characters do you want to be in your password?'));
 
-  var (userChoice < 8 || userChoice > 128) {
-    alert('Please type in a number between 8 and 128');
-
-    return;
-  } else if (isNaN(characters)) {
-    alert('Please type a valid number')
-    return;
-
-}
+  if (userChoice < 8 || userChoice > 128) {
+      window.alert('Please type in a number between 8 and 128');
+      return;
+  } else if (isNaN(userChoice)) {
+      window.alert('Please type a valid number');
+      return;
+  }
 // Asking user to pick a number between 8 and 128
 // If it's less than 8 (< 8) or greater than 128 (> 128) then a alert box will pop up
 // Not a valid number (isNaN) will pop in return
@@ -48,10 +37,36 @@ var lowercaseQuestion = confirm('Do you want lowercase characters? Click OK for 
 var uppercaseQuestion = confirm('Do you want uppercase characters? Click OK for yes, cancel for no')
 
 
+if (numberQuestion) {
+  randomPassword.push(numbers);
+}
+if (symbolQuestion) {
+  randomPassword.push(symbols);
+}
+if (lowercaseQuestion) {
+  randomPassword.push(lowercaseLetters);
+}
+if (uppercaseQuestion) {
+  randomPassword.push(uppercaseLetters);
+}
+else if (!numberQuestion && !symbolQuestion && !lowercaseQuestion && !uppercaseLetters) {
+  window.alert('Please select at least one character type')
+  return;
+}
 
 
+// Saving users input from prompt questions
+var passwordChoice = {
+  userChoice: userChoice,
+  numberQuestion: numberQuestion,
+  symbolQuestion: symbolQuestion,
+  lowercaseQuestion: lowercaseQuestion,
+  uppercaseQuestion: uppercaseQuestion
+}
+return passwordChoice;
 
-
+// If user picks numbers, symbols, lowercase letters or uppercase letters it will be in their new password
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -61,11 +76,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
